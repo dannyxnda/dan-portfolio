a074cb57160383c3bb91d32e8a5ec5d69e82403b
DAN <ducanh.danny@gmail.com>
2021-09-16 19:26:12 +0700

    Add meta tag and images

--- components/templates/layout/index.tsx
+++ components/templates/layout/index.tsx
@@ -11,6 +11,7 @@ type Props = {
   title?: string
   description: string
   icon?: string
+  altIcon?: string
   children: React.ReactNode
 }
 
@@ -20,15 +21,25 @@ const Layout = ({
   title,
   description,
   icon,
+  altIcon,
   children,
 }: Props) => {
+  const { NEXT_PUBLIC_DOMAIN } = process.env
   return (
     <StyledLayout>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
         <link rel="icon" type="image/svg+xml" href={icon} />
-        <link rel="alternate icon" href="/images/DannyLogo.png" />
+        <link rel="alternate icon" href={altIcon} />
+
+        <meta property="og:url" content={NEXT_PUBLIC_DOMAIN} />
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={altIcon} />
+        <meta property="og:locale" content="vi" />
       </Head>
 
       {isHeaderShown && <Header />}
@@ -47,6 +58,7 @@ Layout.defaultProps = {
   description:
     'Front-end Developer | Javascript | Typescript | React.js | Next.js',
   icon: '/images/danny_logo_noborder.svg',
+  altIcon: '/images/danny_logo_noborder.png',
 }
 
 export { Layout }
