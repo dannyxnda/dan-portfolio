85f10eee5e681b98d45f1c06fd0357bfa881d78d
DAN <ducanh.danny@gmail.com>
2021-10-17 21:50:49 +0700

    Change description

--- components/templates/layout/index.tsx
+++ components/templates/layout/index.tsx
@@ -74,7 +74,7 @@ Layout.defaultProps = {
   isHeaderShown: true,
   isFooterShown: true,
   title: 'Duc Anh Nguyen | dannyxnda',
-  description: 'Duc Anh Nguyen | Danny | dannyxnda',
+  description: 'Duc Anh Nguyen | Front-end Developer',
   // icon: '/images/danny_logo_noborder.png',
   altIcon: '/images/danny_logo_noborder.png',
 }
